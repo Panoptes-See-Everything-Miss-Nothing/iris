894b3197ddf0c60a74b3cd25b38b772e39b3e01a
Vaibhav Kakade <vk.appledore@gmail.com>
2026-01-13 13:47:30 +0000

    adding logic to store cvss data into models

--- src/utils/nvd_parse.py
+++ src/utils/nvd_parse.py
@@ -1,6 +1,6 @@
 import json
 
-from src.utils.nvd_utils import get_cpe_data
+from src.utils.nvd_utils import get_cpe_data, create_version_dictionary
 
 
 def read_from_json(file_path) -> str | None:
@@ -55,13 +55,21 @@ def parse_data(data) -> list:
                         for obj in cve_obj.get("descriptions")
                         if obj["lang"] == "en"
                     ),
-                    "cpe_nodes": cpe_data,
                 }
             )
-            nvd_data.append(cve_object)
-            import pprint
+            metrics = cve_obj.get("metrics")
+            if metrics:
+                cvss_v2 = metrics.get("cvssMetricV2")
+                if cvss_v2:
+                    cve_object["cvss_v2"] = create_version_dictionary(cvss_v2)
 
-            pprint.pp(cve_object)
+                cvss_v3 = metrics.get("cvssMetricV31")
+                if cvss_v3:
+                    cve_object["cvss_v3"] = create_version_dictionary(cvss_v3)
+            nvd_data.append(cve_object)
+            # import pprint
+
+            # pprint.pp(cve_object)
         # else:
         #     print(f"No CPE data found for {cve}")
     return nvd_data
--- src/utils/nvd_utils.py
+++ src/utils/nvd_utils.py
@@ -1,45 +1,45 @@
 import re
+from typing import Dict, List
+
+from src.constants import (
+    CVSSSeverity,
+    UserInteraction,
+    AvailabilityImpact,
+    AttackVector,
+    PrivilegesRequired,
+    AttackComplexity,
+)
 
 
-# def create_version_dictionary(data):
-# source = data.get("source")
+def create_version_dictionary(cvss_obj_list: List[Dict]) -> Dict:
+    cvss_obj = cvss_obj_list[0]
+    cvss_data = cvss_obj.get("cvssData")
+    result = {}
 
-# cvss_data = data.get("cvssData")
-# version = cvss_data.get("version")
-# base_score = cvss_data.get("baseScore")
-# access_vector = cvss_data.get("accessVector")
-# access_complexity = cvss_data.get("accessComplexity")
-# authentication = cvss_data.get("authentication")
-# confidentiality_impact = cvss_data.get("confidentialityImpact")
-# integrity_impact = cvss_data.get("integrityImpact")
-# availability_impact = cvss_data.get("availabilityImpact")
+    if cvss_data:
+        result["base_score"] = cvss_data.get("baseScore")
+        result["attack_vector"] = AttackComplexity.from_raw(
+            AttackVector.from_raw(cvss_data.get("attackVector"))
+        )
+        result["access_complexity"] = cvss_data.get("accessComplexity")
+        result["authentication"] = cvss_data.get("authentication")
+        result["confidentiality_impact"] = cvss_data.get("confidentialityImpact")
+        result["integrity_impact"] = cvss_data.get("integrityImpact")
+        result["availability_impact"] = AvailabilityImpact.from_raw(
+            cvss_data.get("availabilityImpact")
+        )
+        result["base_severity"] = CVSSSeverity.from_raw(cvss_obj.get("baseSeverity"))
+        result["privileges_required"] = PrivilegesRequired.from_raw(
+            cvss_obj.get("privilegesRequired")
+        )
+        result["exploitability_score"] = cvss_obj.get("exploitabilityScore")
+        result["impact_score"] = cvss_obj.get("impactScore")
+        result["vector_string"] = cvss_data.get("vectorString")
+        result["user_interaction"] = UserInteraction.from_raw(
+            cvss_data.get("userInteraction")
+        )
 
-# base_severity = data.get("baseSeverity")
-# exploitabilityScore = data.get("exploitabilityScore")
-# impactScoredata = data.get("impactScore")
-
-# return {
-#     "source": source,
-#     "version": version,
-#     "base_score": base_score,
-#     "access_vector": access_vector.lower() if access_vector else None,
-#     "access_complexity": access_complexity.lower() if access_complexity else None,
-#     "authentication": authentication.lower() if authentication else None,
-#     "confidentiality_impact": (
-#         confidentiality_impact.lower() if confidentiality_impact else None
-#     ),
-#     "integrity_impact": integrity_impact.lower() if integrity_impact else None,
-#     "availability_impact": (
-#         availability_impact.lower() if availability_impact else None
-#     ),
-#     "base_severity": base_severity.lower() if base_severity else None,
-#     "exploitabilityScore": exploitabilityScore,
-#     "impactScoredata": impactScoredata,
-# }
-# return {
-#     "source": source,
-#     "base_score": base_score,
-# }
+    return result
 
 
 def get_cpe_data(configurations):
