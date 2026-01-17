f733f6b95f008d85ac45b100e7e6795ed39782dd
Vaibhav Kakade <vk.appledore@gmail.com>
2026-01-17 10:29:02 +0000

    corrected imports, removed unwanted columns, added print stmts

--- src/services/cvss_scores.py
+++ src/services/cvss_scores.py
@@ -1,10 +1,9 @@
-from src.models.cvss import CVSSv2, CVSSv3
+from src.models.cvss import CVSSv2, CVSSv31
 
 
 def parse_cvss_v2(cve_id, v2):
     cvss_v2 = CVSSv2(
         cve_id=cve_id,
-        version=v2.get("version"),
         base_score=v2.get("baseScore"),
         base_severity=v2.get("baseSeverity"),
         access_vector=v2.get("accessVector"),
@@ -23,13 +22,13 @@ def parse_cvss_v2(cve_id, v2):
         obtain_other_privilege=v2.get("obtainOtherPrivilege"),
         user_interaction_required=v2.get("userInteractionRequired"),
     )
+    print("CVSS2", v2)
     return cvss_v2
 
 
 def parse_cvss_v3(cve_id, v3):
-    cvss_v3 = CVSSv3(
+    cvss_v3 = CVSSv31(
         cve_id=cve_id,
-        version=v3.get("version"),
         base_score=v3.get("baseScore"),
         base_severity=v3.get("baseSeverity"),
         attack_vector=v3.get("attackVector"),
@@ -44,4 +43,5 @@ def parse_cvss_v3(cve_id, v3):
         impact_score=v3.get("impactScore"),
         vector_string=v3.get("vectorString"),
     )
+    print("CVSS3", v3)
     return cvss_v3
