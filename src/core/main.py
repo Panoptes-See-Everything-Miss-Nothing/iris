dfbff9681222e22eff6d9d23cc23e7062a2e9a64
Vaibhav Kakade <vk.appledore@gmail.com>
2026-02-17 17:06:29 +0000

    Added API call method

--- src/core/main.py
+++ src/core/main.py
@@ -1,16 +1,28 @@
-from src.utils.nvd_parse import read_from_json, parse_data
+import sys
+
+from src.utils.nvd_parse import read_from_nvd_api, parse_data
 from src.services.cve_importer import save_cves
 
-from src.settings import FIXTURES_FILE
+from src.settings import CVE_URL
 
 
 if __name__ == "__main__":
-    data = read_from_json(FIXTURES_FILE)
+    combined_data = dict()
 
-    if not data:
-        pass
-    cve_objects = parse_data(data)
-    # print(cve_objects)
+    # json_data = read_from_json(FIXTURES_FILE)
+    # if json_data:
+    #     combined_data.update(json_data)
+
+    api_data = read_from_nvd_api(CVE_URL)
+    if api_data:
+        combined_data.update(api_data)
+
+    if not combined_data:
+        print("No data to parse")
+        sys.exit(1)
+
+    cve_objects = parse_data(combined_data)
+    cve_objects = []
     if not cve_objects:
         print("No new CVEs found")
     else:
