dd6f8b3c44494767e9f353201cd3f91bf135622c
Vaibhav Kakade <vk.appledore@gmail.com>
2026-02-18 14:47:29 +0000

    moved cve url to settings

--- src/settings.py
+++ src/settings.py
@@ -10,17 +10,18 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DATABASE = os.getenv("DATABASE")
+NVD_API_KEY = os.getenv("NVD_API_KEY")
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 FIXTURES_DIR = PROJECT_ROOT / "src/fixtures"
 DEFAULT_FIXTURES_FILE = FIXTURES_DIR / "nvdcve_2.0.json"
 FIXTURES_FILE = os.getenv("FIXTURES_FILE", str(DEFAULT_FIXTURES_FILE))
-
 DATABASE_URL = (
     f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DATABASE}"
 )
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+CVE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
 
 
 def get_db():
