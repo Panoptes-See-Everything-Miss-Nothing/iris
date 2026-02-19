ec458c30e1fdfefc7d6ab712bc153dccd8230ae4
Vaibhav Kakade <vk.appledore@gmail.com>
2026-02-19 16:06:32 +0000

    replaced with logger stmts

--- src/utils/nvd_parse.py
+++ src/utils/nvd_parse.py
@@ -1,8 +1,8 @@
 import json
 import asyncio
 import aiohttp
-from typing import Dict, List
 from math import ceil
+from typing import Dict, List
 
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
@@ -13,15 +13,7 @@ from src.settings import SessionLocal, CVE_URL, FIXTURES_FILE, NVD_API_KEY
 
 import logging
 
-LOG_FILE = "nvd_client.log"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()],
-)
-
-logger = logging.getLogger("nvd_client")
+logger = logging.getLogger(__name__)
 
 
 def read_from_json() -> Dict | None:
@@ -30,9 +22,11 @@ def read_from_json() -> Dict | None:
             data = json.load(file)
             return data
     except FileNotFoundError:
-        print(f"Please check path correctly: {FIXTURES_FILE}")
-    except Exception as e:
-        print(f"Could not parse file: {e}")
+        logger.exception(
+            "Please check path correctly", extra={"fixtures_file": FIXTURES_FILE}
+        )
+    except Exception:
+        logger.exception("Could not parse file")
 
 
 async def fetch_page(session: aiohttp.ClientSession, params: Dict, page: int) -> List:
@@ -48,25 +42,18 @@ async def fetch_page(session: aiohttp.ClientSession, params: Dict, page: int) ->
 
                 response.raise_for_status()
                 data = await response.json()
-                print(f"Fetched page {page}")
                 logger.info(f"Fetched page {page}")
                 return data.get("vulnerabilities", [])
 
-        except aiohttp.ClientError as e:
-            print(f"Error occured for page {page}: {e}")
+        except aiohttp.ClientError:
+            logger.exception("Error occured for page", extra={"page": page})
             return []
-    print(f"Failed page {page} after retries")
+    logger.error("Failed after multiple retries", extra={"page": page})
     return []
 
 
-# Sequential execution:
-#   Total Records 273042
-#   Total Time taken: 0:47:57.390410
-#   With ~30 pages 429 error code
-
-
 async def read_from_nvd_api() -> Dict | None:
-    print("Reading from API", CVE_URL)
+    logger.info("Reading from API", extra={"cve_url": CVE_URL})
     page_size = 2000
 
     header = {"apiKey": NVD_API_KEY}
@@ -78,13 +65,13 @@ async def read_from_nvd_api() -> Dict | None:
             ) as response:
                 response.raise_for_status()
                 data = await response.json()
-        except aiohttp.ClientError as e:
-            print(f"Intial API call Failed: {e}")
+        except aiohttp.ClientError:
+            logger.exception("Intial API call Failed")
             return
 
         vulnerabilities = []
         total_results = data.get("totalResults", 0)
-        print(f"Total Records reported by API: {total_results}")
+        logger.info("Total Records reported by API %s", total_results)
 
         if not total_results:
             return {"vulnerabilities": vulnerabilities}
@@ -92,11 +79,11 @@ async def read_from_nvd_api() -> Dict | None:
         vulnerabilities.extend(data.get("vulnerabilities", []))
 
         page_count = ceil(total_results / page_size)
-        print(f"Total pages {page_count}")
+        logger.info("Total pages %s", page_count)
 
         tasks = []  # prepare concurrent tasks for remaining pages
         for page in range(1, page_count):
-            print(f"Fetching from page {page}/{page_count}")
+            logger.info("Fetching from page %s/%s", page, page_count)
             params = {"resultsPerPage": page_size, "startIndex": page * page_size}
 
             tasks.append(fetch_page(session, params, page))
@@ -106,18 +93,18 @@ async def read_from_nvd_api() -> Dict | None:
             for page_data in results:
                 vulnerabilities.extend(page_data)
 
-    print(f"Total Records {len(vulnerabilities)}")
+    logger.info("Total Records %s", len(vulnerabilities))
     return {"vulnerabilities": vulnerabilities}
 
 
 def write_to_json(nvd_data, file_path) -> str | None:
     try:
-        print("Writing to file")
+        logger.info("Writing to file")
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump(nvd_data, f, indent=4, ensure_ascii=False)
-            print(f"Data dumped in {file_path}")
+            logger.info("Data dumped in %s", file_path)
     except FileNotFoundError:
-        print(f"Could not write to {file_path}")
+        logger.exception("Could not write to %s", file_path)
 
 
 def get_existing_cve_ids() -> set | None:
@@ -125,7 +112,7 @@ def get_existing_cve_ids() -> set | None:
     Fetch all existing CVE IDs from DB.
     Returns set of IDs or None on failure.
     """
-    print("Fetching Existing CVEs")
+    logger.info("Fetching Existing CVEs")
     try:
         with SessionLocal() as db:
             db.execute(select(1)).scalar()
@@ -134,14 +121,14 @@ def get_existing_cve_ids() -> set | None:
             result = db.execute(select(CVE.cve_id)).all()
             existing_ids = {row[0] for row in result}
 
-            print(f"Found {len(existing_ids)} existing CVEs in DB")
+            logger.info("Found %s existing CVEs in DB", len(existing_ids))
             return existing_ids
 
-    except (IntegrityError, SQLAlchemyError) as e:
-        print(f"Database error while fetching existing CVEs: {e}")
+    except (IntegrityError, SQLAlchemyError):
+        logger.exception("Database error while fetching existing CVEs")
         return None
-    except Exception as e:
-        print(f"Unexpected error during DB check: {e}")
+    except Exception:
+        logger.exception("Unexpected error during DB check")
         return None
 
 
@@ -149,7 +136,7 @@ def parse_data(data) -> list | None:
     existing_cves = get_existing_cve_ids()
 
     if existing_cves is None:
-        print("Connot proceed database connection failed")
+        logger.error("Connot proceed database connection failed")
         return None
 
     nvd_data = []
@@ -157,18 +144,18 @@ def parse_data(data) -> list | None:
         cve_obj = _.get("cve")
         cve_id = cve_obj.get("id")
         if not cve_id:
-            print("CVE not found. Skipping object")
+            logger.info("CVE not found. Skipping object")
             continue
 
         if cve_id in existing_cves:
-            print(f"{cve_id} already exists, skipping")
+            logger.info("CVE already exists, skipping", extra={"cve_id": cve_id})
             continue
 
         cve_object = {"cve": cve_id}
         if configurations := cve_obj.get("configurations"):
             cpe_data = get_cpe_data(configurations)
             if not cpe_data:
-                print(f"Failed to fetch CPE data for {cve_id}")
+                logger.error("Failed to fetch CPE data for", extra={"cve_id": cve_id})
                 continue
 
             cve_object.update(
@@ -202,5 +189,5 @@ def parse_data(data) -> list | None:
             # import pprint
             # pprint.pp(cve_object)
         else:
-            print(f"No CPE data found for {cve_id}")
+            logger.info(f"No CPE data found for {cve_id}")
     return nvd_data
