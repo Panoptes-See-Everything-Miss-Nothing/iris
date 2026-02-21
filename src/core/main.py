4ac44437dbe87c2dc94e56236422ebaab87ce604
Vaibhav Kakade <vk.appledore@gmail.com>
2026-02-21 12:00:02 +0000

    Cleaner method calls, async API feed parsing, logger enhancment & import correction

--- src/core/main.py
+++ src/core/main.py
@@ -1,43 +1,77 @@
-import sys
 import asyncio
 import logging
+from typing import Any
 
+from src.utils.nvd_parser import read_from_json, read_from_nvd_api
+from src.utils.nvd_feed_scraper import get_json_feed_files
 
-from src.utils.nvd_parse import read_from_nvd_api, parse_data
-from src.services.cve_importer import save_cves
+from src.utils.cve_parser import parse_data
+from src.crud.cve_importer import save_cves
 from src.logger_config import setup_logging
 
 setup_logging()
-
 logger = logging.getLogger(__name__)
+
+
+def process_data(cve_objects: list[dict[str, Any]]):
+    result = save_cves(cve_objects)
+
+    if result:
+        logger.info("Save successful")
+    elif result is None:
+        logger.info("No data present")
+    else:
+        logger.error("Failed to save CVE objects")
+
+
+def run_json_feed():
+    logger.info("Running JSON parsing")
+
+    json_feed_files = get_json_feed_files()
+    if not json_feed_files:
+        logger.error("No files to parse")
+        return
+    else:
+        for file_path in json_feed_files:
+            json_data = read_from_json(file_path)
+            if json_data is None:
+                logger.warning("Failed to read %s", file_path)
+                continue
+
+            cve_objects = parse_data(json_data)
+            if cve_objects is None:
+                logger.error("Parsing failed for %s", file_path)
+            elif not cve_objects:
+                logger.info("No new CVEs found in %s", file_path)
+            else:
+                process_data(cve_objects)
+    logger.info("Running JSON Feed Completed")
+
+
+async def run_api_feed():
+    logger.info("Running API Feed")
+
+    api_data = await read_from_nvd_api()
+    if api_data is None:
+        logger.error("No data from APIs")
+        return
+    else:
+        cve_objects = parse_data(api_data)
+        if cve_objects is None:
+            logger.error("Parsing failed for API data")
+        elif not cve_objects:
+            logger.info("No new CVEs found")
+        else:
+            process_data(cve_objects)
+    logger.info("Running API Feed Completed")
+
+
+async def main():
+    run_json_feed()
+    await run_api_feed()
 
 
 if __name__ == "__main__":
     logger.info("Execution started")
-    combined_data = dict()
-
-    # json_data = read_from_json()
-    # if json_data:
-    #     combined_data.update(json_data)
-
-    api_data = asyncio.run(read_from_nvd_api())
-    if api_data:
-        combined_data.update(api_data)
-
-    if not combined_data:
-        print("No data to parse")
-        sys.exit(1)
-
-    cve_objects = parse_data(combined_data)
-    cve_objects = []
-    if not cve_objects:
-        print("No new CVEs found")
-    else:
-        result = save_cves(cve_objects)
-        if result:
-            print("Save successful")
-        elif result is None:
-            print("No data present")
-        else:
-            print("Failed to save CVE objects")
-    logger.info("Execution Stooped")
+    asyncio.run(main())
+    logger.info("Execution Stopped")
